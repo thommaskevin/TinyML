41677c7013ef22c70a883cb7edad53425c268349
Thommas Kevin <thommas.flores@cear.ufpb.br>
2023-12-25 23:20:41 -0300

    XGBClassifier

---
+++ 03_XGBoost/XGBoostClassifier/XGBoostClassifier.ino
@@ -0,0 +1,29 @@
+// Arduino sketch
+#include "XGBClassifier.h"
+
+Eloquent::ML::Port::XGBClassifier classifier;
+
+void setup() {
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  float X_1[] = {5.1, 3.5, 1.4, 0.2};
+  int result_1 = classifier.predict(X_1);
+  Serial.print("Result of predict with input X1:");
+  Serial.println(result_1);
+  delay(2000);
+
+  float X_2[] = {6.2, 2.2, 4.5, 1.5};
+  int result_2 = classifier.predict(X_2);
+  Serial.print("Result of predict with input X2:");
+  Serial.println(result_2); 
+  delay(2000);
+
+  float X_3[] = {6.1, 3.0, 4.9, 1.8};
+  int result_3 = classifier.predict(X_3);
+  Serial.print("Result of predict with input X3:");
+  Serial.println(result_3);
+  delay(2000);
+}

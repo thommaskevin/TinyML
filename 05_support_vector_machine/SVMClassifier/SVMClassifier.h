82ba0032f850bac3ccafd18a73d1b608e002110a
Thommas Kevin <thommas.flores@cear.ufpb.br>
2024-01-10 12:19:30 -0300

    last update

--- 05_support_vector_machine/SVMClassifier/SVMClassifier.h
+++ 05_support_vector_machine/SVMClassifier/SVMClassifier.h
@@ -12,16 +12,14 @@ namespace Eloquent {
                         float kernels[3] = { 0 };
                         float decisions[1] = { 0 };
                         int votes[2] = { 0 };
-                        kernels[0] = compute_kernel(x,   3.3  , 1.7  , 0.5 );
-                        kernels[1] = compute_kernel(x,   3.4  , 1.9  , 0.2 );
-                        kernels[2] = compute_kernel(x,   2.5  , 3.0  , 1.1 );
-                        float decision = -0.903345042107;
-                        decision = decision - ( + kernels[0] * -0.743493539534  + kernels[1] * -9.21056e-07 );
-                        decision = decision - ( + kernels[2] * 0.74349446059 );
-
+                        kernels[0] = compute_kernel(x,   3.4  , 1.9  , 0.2 );
+                        kernels[1] = compute_kernel(x,   3.3  , 1.7  , 0.5 );
+                        kernels[2] = compute_kernel(x,   2.4  , 3.3  , 1.0 );
+                        float decision = -0.833910342285;
+                        decision = decision - ( + kernels[0] * -0.31945543931  + kernels[1] * -0.240101867421 );
+                        decision = decision - ( + kernels[2] * 0.559557306731 );
                         return decision > 0 ? 0 : 1;
                     }
-
                 protected:
                     /**
                     * Compute kernel between feature vector and support vector.
@@ -31,11 +29,9 @@ namespace Eloquent {
                         va_list w;
                         va_start(w, 3);
                         float kernel = 0.0;
-
                         for (uint16_t i = 0; i < 3; i++) {
                             kernel += x[i] * va_arg(w, double);
                         }
-
                         return kernel;
                     }
                 };

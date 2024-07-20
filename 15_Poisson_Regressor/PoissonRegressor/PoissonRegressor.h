be779ce50d2d2317bbca74a599b3d08d97176829
Thommas Kevin <thommas.flores@cear.ufpb.br>
2024-07-19 22:37:36 -0300

    ElasticNet

--- 15_Poisson_Regressor/PoissonRegressor/PoissonRegressor.h
+++ 15_Poisson_Regressor/PoissonRegressor/PoissonRegressor.h
@@ -1,23 +1,5 @@
-namespace Eloquent {
-namespace ML {
-namespace Port {
-class PoissonRegressor {
-public: 
-
-float predict(float *x) { 
-float predict = 0;
-float coefficients[8] = {0.09042590463658687, -0.09696434781840536, 0.3653828589531427, 0.2711244659564742, 0.10702971852706969, -0.2758301947563339, 0.2689055595556031, 0.458699680210232};
-
-    float z = 3.517012193074436;
-    for (int i = 0; i < 8; i++)
-     {
-       z += coefficients[i] * x[i];
-     }
-
-predict =  exp(z);  
-return predict; 
-} 
-};
-}
-}
+#include <math.h>
+double score(double *input)
+{
+    return exp(1.7347124654302846 + input[0] * 0.011406244946132144 + input[1] * 0.01010646886054758 + input[2] * 0.0028201461971878914);
 }
---
+++ 17_ElasticNet/ElasticNet/ElasticNet.h
@@ -0,0 +1,3 @@
+double score(double * input) {
+    return 0.21407132576818277 + input[0] * 0.0 + input[1] * 0.0 + input[2] * 0.048847064561809886;
+}
